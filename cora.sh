9808fbc1f244ebf7a464900a3ab223419c5f106e
Alfredo Deza <alfredo@deza.pe>
2014-03-05 20:04:15 -0500

    better shell detection and working exec

--- cora.sh
+++ cora.sh
@@ -8,49 +8,87 @@
 # As opposed to manually having to call ``export`` with the location
 #
 
-zdotdir=''
-bashrc=''
-
 function vre_path {
-    DIR="$HOME/.cora/$1"
+    DIR="$HOME/.cora/$CORA_USERNAME"
     if [ -d "$DIR" ]; then
         echo "$DIR"
     fi
 }
 
 function shell_flags {
-    if [ -n "$BASH" ]
-    then
-        echo "-i --rcfile $bashrc"
-    elif [ -n "$ZSH_VERSION" ]
+    if [ -n "$ZSH_VERSION" ]
     then
         echo "-i"
+
+    elif [ -n "$BASH_VERSION" ]
+    then
+        echo "-i --rcfile $(bashrc)"
     fi
 }
+
 
 function shell_cmd {
-    if [ -n "$BASH" ]
+    # Attempt to get the right command to call for exec. It is actually tricky
+    # to determine if we are BASH or ZSH because if we are on ZSH and are
+    # executing the current script via /bin/sh which can be aliased to BASH
+    # then we will fail to correctly set the right commands.
+    #
+    # Finally, we cannot fully rely on $SHELL because the actual path for that
+    # shell might be a bit different so we need to call `which` on it to get
+    # the right PATH to the executable.
+    #
+    if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]
     then
-        echo "$SHELL "
-    elif [ -n "$ZSH_VERSION" ]
+        executable=`which zsh`
+        export ZDOTDIR="$(zdotdir)"
+        echo "$executable -i"
+    elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]
     then
-        echo "ZDOTDIR=$zdotdir $SHELL "
+        executable=`which bash`
+        echo "$executable $(shell_flags)"
     fi
 }
 
-vre_bin="$(vre_path)$1/runtime/bin"
 
-#if [ ! -e "$vre_bin" ]; then
-#    echo "Did not find virtual runtime directory: $vre_bin"
-#else
-#    export VIM=$vre_bin/vim
-#    exec $SHELL
-#fi
+function success {
+    if [ -e "$(vre_path)/.done" ]; then
+        echo 1
+    fi
+}
+
+
+function zdotdir {
+    echo "$(vre_path)/rc"
+}
+
+
+function bashrc {
+    echo "$(vre_path)/rc/.bashrc"
+}
+
 
 if [ "$#" = 1 ]; then
     export CORA_USERNAME="$1"
 fi
+
 if [ "$#" = 2 ]; then
     export CORA_URL="$1"
     export CORA_USERNAME="$2"
+fi
+
+vre_bin="$(vre_path)/bin"
+
+if [ -n "$(success)" ]; then
+
+    if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]
+    then
+        executable=`which zsh`
+        export ZDOTDIR="$(zdotdir)"
+        exec $executable -i
+    elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]
+    then
+        executable=`which bash`
+        exec $executable $(shell_flags)
+    fi
+
 fi
